45bcd5b88bf7e7399d3e2b858fe9517dfa906cc8
BayuHyuuga <Bayu.Budianto@bni.co.id>
2024-10-01 10:33:44 +0700

    Add project files.

---
+++ WebApplication1/ModelA.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1
+{
+    [ModelBinder]
+    public class ModelA
+    {
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Value { get; set; }
+    }
+}
